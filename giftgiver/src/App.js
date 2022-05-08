a35654c6f8c954463ada94907fe3ba02874ce77b
Miraaj <miraajkadam@gmail.com>
2022-05-08 21:14:15 +0530

    Added tests

--- giftgiver/src/App.js
+++ giftgiver/src/App.js
@@ -1,9 +1,34 @@
-const App = () => {
-  return (
-    <div>
-      <h2>Gift Giver</h2>
-    </div>
-  );
-};
+import { Component } from 'react'
+import { Button } from 'react-bootstrap'
 
-export default App;
+export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      gifts: [],
+    }
+  }
+
+  addGift = () => {
+    const { gifts } = this.state
+    const ids = gifts.map(gift => gift.id)
+    const max_id = ids.length > 0 ? Math.max(...ids) : 0
+
+    gifts.push({ id: max_id + 1 })
+
+    this.setState({ gifts })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Gift Giver</h2>
+        
+        <Button onClick={this.addGIft} className="btn-add">
+          Add Gift
+        </Button>
+      </div>
+    )
+  }
+}
--- giftgiver/src/__tests__/App.js
+++ giftgiver/src/__tests__/App.js
@@ -1,9 +1,21 @@
-import { shallow } from "enzyme";
-import React from "react";
-import App from "../App";
+import { shallow } from 'enzyme'
+import React from 'react'
+import App from '../App'
 
-const app = shallow(<App />);
+const app = shallow(<App />)
 
-it("renders correctly", () => {
-  expect(app).toMatchSnapshot();
-});
+it('renders correctly', () => {
+  expect(app).toMatchSnapshot()
+})
+
+it('initializes the `state` with an empty list of gifts', () => {
+  expect(app.state().gifts).toEqual([])
+})
+
+it('adds a new gift to `state` when clicking the `add gift` button', () => {
+  app.find('.btn-add').simulate('click')
+
+  expect(app.state().gifts).toEqual([{ id: 1 }])
+})
+
+// it('adds a new fit')
